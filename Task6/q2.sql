07e64f9059af8aecc6fbad8087fcf53c5b133f9c
sanika-dpandit <143989050+sanika-dpandit@users.noreply.github.com>
2025-08-13 09:30:30 +0400

    Create q2.sql

---
+++ Task6/q2.sql
@@ -0,0 +1,9 @@
+SELECT
+    YEAR(order_date) AS order_year,
+    MONTH(order_date) AS order_month,
+    SUM(amount) AS total_revenue,
+    COUNT(DISTINCT order_id) AS total_orders
+FROM online_sales
+GROUP BY order_year, order_month
+ORDER BY total_revenue DESC
+LIMIT 3;
